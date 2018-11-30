82673dfb2b9822824d6e1bceffcff4a160c016c7
Frank Foerster <Frank.Foerster@ime.fraunhofer.de>
2018-11-30 14:14:29 +0100

    HMMsearch for created hmms and files in assembly folder

--- run.sh
+++ run.sh
@@ -14,3 +14,22 @@ do
     date +"[%Y-%m-%d %H:%M:%S] Finished '${i}'"
 done
 
+# Search each hmm against each file from assembly folder
+# Get each hmm created in first step
+for hmmfile in $(find hmms/ -type f -name "*.hmm")
+do
+    # run hmmsearch for each file in assemblies ending in *.fa...
+    for assemblyfile in $(find assemblies/ -type f | grep -P "\.fa[^.]*$")
+    do
+	date +"[%Y-%m-%d %H:%M:%S] Started hmmseach for '${hmmfile}' vs '${assemblyfile}'"
+	OUTFILE_BASENAME=hmmsearch/"${hmmfile}"_vs_"${assemblyfile}"
+	hmmsearch \
+	    --tblout "${OUTFILE_BASENAME}".tblout \
+	    --domtblout "${OUTFILE_BASENAME}".domtblout \
+	    --pfamtblout "${OUTFILE_BASENAME}".pfamtblout \
+	    "${hmmfile}" \
+	    "${assemblyfile}" \
+	    > "${OUTFILE_BASENAME}".out
+	date +"[%Y-%m-%d %H:%M:%S] Finished hmmseach for '${hmmfile}' vs '${assemblyfile}'"
+    done
+done
